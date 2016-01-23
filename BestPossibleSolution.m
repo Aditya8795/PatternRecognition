0e80c91d6a817d6f8cf3ee93c70112248266c878
Aditya A Prasad <adityaprasadiskool@gmail.com>
2016-01-23 16:14:16 +0530

    Add code to plot Column Space of A
    In case of A being a rectangular matrix of size m*n
    A solution to Ax = b, exist for those b which lie in the column space
    of A.
    
    The code which I have added in this commit, plots this column space
    for m=3 and n=2. This is a subspace in R^3. The example I have taken
    from gilbert strang's book turns out to have only 2 pivots (You can try
    verifying using rref(A)). Thus We get the column space to be a plane.
    
    Note that [0;0;0], and both columns of A lie in the
    subspace/column space. Next I hope to be able to plot a b which doesnt
    lie on this plane and show that its projection onto this plane is the
    best **approximate** solution to the Ax=b problem.
    
    Signed-off-by: Aditya A Prasad <adityaprasadiskool@gmail.com>

---
+++ BestPossibleSolution.m
@@ -0,0 +1,27 @@
+% The Objective of this mat file is to graphically show how the Projection
+% of the vector b onto A's column space gives the best solution to Ax = b
+% In the case of a Rectangular or Singular b.
+
+% Let this be a m*n matrix (m equations & n unknowns system)
+A = [1 0; 5 4; 2 4]; % 3*2
+A_T = A';
+
+column1 = A_T(1,:)';
+column2 = A_T(2,:)';
+normal = cross(column1,column2);
+
+%# a plane is a*x+b*y+c*z+d=0
+%# [a,b,c] is the normal. Thus, we have to calculate
+%# d and we're set
+d = -A_T(1,:)*normal;
+
+%# create x,y
+[xx,yy]=ndgrid(0:100,0:100);
+
+%# calculate corresponding z
+z = (-normal(1)*xx - normal(2)*yy - d)/normal(3);
+
+%# plot the surface
+figure
+% the points that b can take such that there exists a solution for Ax = b
+surf(xx,yy,z) 
